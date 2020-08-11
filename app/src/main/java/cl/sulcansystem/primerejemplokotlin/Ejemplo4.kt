34582ff52449dce8415b4b3669d1506cdc77d14e
Franklin Jiménez <franklin.jimenez.p@gmail.com>
2020-08-11 16:29:22 -0400

    commit1

---
+++ app/src/main/java/cl/sulcansystem/primerejemplokotlin/Ejemplo4.kt
@@ -0,0 +1,4 @@
+package cl.sulcansystem.primerejemplokotlin
+data class Libro(val nombre:String, val ISBN:Int, val añoPublic: String, val editorial:String, val cantPag:Int, val precio:Int, val autor:String, var tipo:String)
+
+
